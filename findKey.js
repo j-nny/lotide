88cfb8bfdcd67140106068bcf35541729d9476b1
Jenny Nguy <j.nguy809@gmail.com>
2019-09-23 21:00:24 +0000

    final changes

--- findKey.js
+++ findKey.js
@@ -9,12 +9,12 @@ const findKey = function(object, callback) {
 }
 
 // console.log(assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
+  // "Blue Hill": { stars: 1 },
+  // "Akaleri":   { stars: 3 },
+  // "noma":      { stars: 2 },
+  // "elBulli":   { stars: 3 },
+  // "Ora":       { stars: 2 },
+  // "Akelarre":  { stars: 3 }
 // }, x => x.stars === 2), 'noma')) // => "noma"
 
 module.exports = findKey;
